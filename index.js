165a531a16083afedf8518a48a77007eeb0925ec
SRAKIB17 <rakibulssc5@gmail.com>
2022-01-31 18:55:07 +0600

    succes

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-setInterval(carousel,1500);
+setTimeout(carousel,50);
 function carousel() {
-     // get imageLength
-     let imageLength =document.getElementsByClassName('carouselImage').length;
-     
-    //  get storeValue
-     let storeValue = document.getElementById('carouselFirstImage').alt;
-     document.getElementById('test').innerHTML = storeValue
-    //  condition apply
-    if (1<= storeValue && storeValue < imageLength) {
-        // hidden image 
-        document.getElementsByClassName('carouselImage')[storeValue-1].style.display = 'none';
+    let storeValue = 0;
+    setInterval(runningCarousel,1500);
+    function runningCarousel() {
+        let imageLength =document.getElementsByClassName('carouselImage').length;
 
-        // showing image 
-        document.getElementsByClassName('carouselImage')[storeValue].style.display = 'block';
+        document.getElementById('test').innerHTML = storeValue
+        //  condition apply
+        if (1<= storeValue && storeValue < imageLength) {
+            // hidden image 
+            document.getElementsByClassName('carouselImage')[storeValue-1].style.display = 'none';
+
+            // showing image 
+            document.getElementsByClassName('carouselImage')[storeValue].style.display = 'block';
+            
+            // incresing value
+            storeValue++
+            // store a new storeValue
+            return storeValue
+        }
+        else if (storeValue == 0) {
+            // hidden image 
+            document.getElementsByClassName('carouselImage')[imageLength-1].style.display = 'none';
+
+            // showing image 
+            document.getElementsByClassName('carouselImage')[0].style.display = 'block';
+
+            // incresing value
+            storeValue++
+            // store a new storeValue
+            return storeValue
+        }
+        else {
+            storeValue = 0;
+            // hidden image 
+            document.getElementsByClassName('carouselImage')[imageLength-1].style.display = 'none';
+            // index no 1
+            document.getElementsByClassName('carouselImage')[storeValue].style.display = 'block';
+
+            // store a new storeValue
+            return storeValue
+        }
+    }
         
-        // incresing value
-        storeValue++
-        // store a new storeValue
-        document.getElementById('carouselFirstImage').alt = storeValue;
-    }
-    else if (storeValue == 0) {
-        // hidden image 
-        document.getElementsByClassName('carouselImage')[imageLength-1].style.display = 'none';
-
-        // showing image 
-        document.getElementsByClassName('carouselImage')[0].style.display = 'block';
-
-        // incresing value
-        storeValue++
-        // store a new storeValue
-        document.getElementById('carouselFirstImage').alt = storeValue;
-    }
-    else {
-        storeValue = 0;
-        // hidden image 
-        document.getElementsByClassName('carouselImage')[imageLength-1].style.display = 'none';
-        // index no 1
-        document.getElementsByClassName('carouselImage')[storeValue].style.display = 'block';
-
-        // store a new storeValue
-        document.getElementById('carouselFirstImage').alt = storeValue;
-    }
     
 }
